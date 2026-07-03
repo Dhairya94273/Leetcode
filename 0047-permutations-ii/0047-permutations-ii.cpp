946ec0b264e797534a6d202ee4a2f3ef4c218292
Dhairya Shah <shahdhairya712@gmail.com>
2026-07-03 08:23:10 +0530

    Time: 1 ms (74.49%), Space: 15.1 MB (17.19%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void getPermutations(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back({nums});
+            return;
+        }
+        unordered_set<int> s;
+        for (int i = idx; i < nums.size(); i++) {
+            if(s.count(nums[i])){
+                continue;
+            }
+            s.insert(nums[i]);
+            swap(nums[idx], nums[i]);
+            getPermutations(nums, idx + 1, ans);
+            swap(nums[idx], nums[i]); // backtracking
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        vector<vector<int>> ans;
+        getPermutations(nums, 0, ans);
+        return ans;
+    }
+};
