84238f866d7899cbb3720605e53c4cc48dd018ee
Dhairya Shah <shahdhairya712@gmail.com>
2026-08-06 22:04:57 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.87%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n =height.size();
-        vector<int> imax(n,0);
-        vector<int> rmax(n,0);
-
-        imax[0] = height[0];
-        rmax[n-1] = height[n-1];
-
-        for(int i=1 ; i<n ; i++){
-            imax[i] = max(imax[i-1] , height[i] );
-        }
-
-        for(int i=n-2;i>=0 ; i--){
-            rmax[i] = max(rmax[i+1] , height[i]);
-        }
-
-        int sum=0;
-        for(int i=0 ; i<n ; i++){
-            sum += min(imax[i] , rmax[i]) - height[i];
+        int ans = 0;
+        int n = height.size();
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
+        int sum = 0;
+        while (l < r) {
+            lmax = max(lmax, height[l]); // calculate an left and Right Boundries for an Perticular bar
+            rmax = max(rmax, height[r]);
+            if (lmax < rmax) { // jo bhi small value he lmax and rmax me voh deciding factor hogi water trap me
+                sum += lmax - height[l];
+                l++;
+            } else {
+                sum += rmax - height[r];
+                r--;
+            }
         }
         return sum;
     }
