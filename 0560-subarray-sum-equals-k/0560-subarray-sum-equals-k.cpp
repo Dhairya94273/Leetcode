3b3a7a828a535638f772395cb3f79ed0e3ce8ac0
Dhairya Shah <shahdhairya712@gmail.com>
2026-08-09 09:51:03 +0530

    Time: 30 ms (96.88%), Space: 46.9 MB (32.09%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> prefixSum(n, 0);
+        prefixSum[0] = nums[0];
+        if(n==0) return 0;
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+        unordered_map<int, int> freq;
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
+        for (int j = 0; j < n; j++) {
+            if (prefixSum[j] == k) {
+                count++;
             }
+            int val = prefixSum[j] - k;
+            if (freq.find(val) != freq.end()) {
+                count += freq[val];
+            }
+
+            freq[prefixSum[j]]++;
         }
         return count;
     }
